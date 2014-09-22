3ac72b6857bb83605a225a3bf9fe1fb2b4a08273
Scott E. Fontenarosa <sefonty@yahoo.com>
2014-09-21 19:02:39 -0500

    Initial project
    made package utdallas.cs5348.batchProcessor
    and main class BatchProcessor.java
    called project batchProject
    made src folder

---
+++ src/utdallas/cs4348/batchProcessor/BatchProcessor.java
@@ -0,0 +1,12 @@
+package utdallas.cs4348.batchProcessor;
+
+public class BatchProcessor
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello Adela! :3");
+	}
+
+}
