657497cfce16398d0a91305f5d8352e0b9245dfe
Scott E. Fontenarosa <sefonty@yahoo.com>
2014-09-21 19:29:58 -0500

    change log statement

--- src/utdallas/cs4348/batchProcessor/BatchProcessor.java
+++ src/utdallas/cs4348/batchProcessor/BatchProcessor.java
@@ -6,7 +6,7 @@ public class BatchProcessor
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Hello Adela! :3");
+		System.out.println("BatchProcessor started");
 	}
 
 }
