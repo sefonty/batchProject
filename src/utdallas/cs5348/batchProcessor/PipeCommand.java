53f8245d37bf2dcb94828607f6c3ec6a52044688
Scott E. Fontenarosa <sefonty@yahoo.com>
2014-09-21 20:35:40 -0500

    templates for Command subclasses added

---
+++ src/utdallas/cs5348/batchProcessor/PipeCommand.java
@@ -0,0 +1,29 @@
+package utdallas.cs5348.batchProcessor;
+
+import org.w3c.dom.Element;
+
+public class PipeCommand extends Command
+{
+
+	@Override
+	public String describe()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void execute(String workingDir)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void parse(Element element)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
